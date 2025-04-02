828cf27dfaaf42923d44b8d23fd73167eb30fd98
mauricio <mauricioalejandrolarrea122@gmail.com>
2025-04-02 07:14:29 -0500

    agregar las tablas a las clases conexion y iconexion

--- lib_repositorios/Interfaces/IConexion.cs
+++ lib_repositorios/Interfaces/IConexion.cs
@@ -12,6 +12,14 @@ namespace lib_repositorios.Interfaces
 
         DbSet<Bodegas>? Bodegas { get; set; }
         DbSet<Productos>? Productos { get; set; }
+        DbSet<Vehiculos>? Vehiculos { get; set; }
+        DbSet<Documentos>? Documentos { get; set; }
+        DbSet<Vehiculos_Documentos>? Vehiculos_Documentos { get; set; }
+        DbSet<Productos_Documentos>? Productos_Documentos { get; set; }
+        DbSet<Empresas>? Empresas { get; set; }
+        DbSet<Empleados>? Empleados { get; set; }
+        DbSet<Roles>? Roles { get; set; }
+
 
 
         EntityEntry<T> Entry<T>(T entity) where T : class;

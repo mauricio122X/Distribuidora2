23f3640b3d7ca4389dd1027ea262c2495905a96f
mauricio <mauricioalejandrolarrea122@gmail.com>
2025-04-22 21:39:10 -0500

    Creacion projecto servicios (Bodegas y token)

---
+++ asp_servicios/Startup.cs
@@ -0,0 +1,54 @@
+﻿using asp_servicios.Controllers;
+using lib_aplicaciones.Implementaciones;
+using lib_aplicaciones.Interfaces;
+using lib_repositorios.Implementaciones;
+using lib_repositorios.Interfaces;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+
+namespace asp_servicios
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public static IConfiguration? Configuration { set; get; }
+
+        public void ConfigureServices(WebApplicationBuilder builder, IServiceCollection services)
+        {
+            services.Configure<KestrelServerOptions>(x => { x.AllowSynchronousIO = true; });
+            services.Configure<IISServerOptions>(x => { x.AllowSynchronousIO = true; });
+            services.AddControllers();
+            services.AddEndpointsApiExplorer();
+            //services.AddSwaggerGen();
+            // Repositorios
+            services.AddScoped<IConexion, Conexion>();
+            // Aplicaciones
+            services.AddScoped<IBodegasAplicacion, BodegasAplicacion>();
+            // services.AddScoped<IAvionesAplicacion, AvionesAplicacion>();
+            // Controladores
+            services.AddScoped<TokenController, TokenController>();
+
+            services.AddCors(o => o.AddDefaultPolicy(b => b.AllowAnyOrigin()));
+        }
+
+        public void Configure(WebApplication app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                //app.UseSwagger();
+                //app.UseSwaggerUI();
+            }
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Error");
+            }
+            app.UseHttpsRedirection();
+            app.UseAuthorization();
+            app.MapControllers();
+            app.UseCors();
+        }
+    }
+}

5be01b8007a37e2a5d3bf9cc61d7b038a4ee6d64
mauricio <mauricioalejandrolarrea122@gmail.com>
2025-04-29 08:53:02 -0500

    Creacion EmpleadosAplicaciones y IEmpleados

---
+++ lib_aplicaciones/Implementaciones/EmpleadosAplicaciones.cs
@@ -0,0 +1,80 @@
+﻿using lib_aplicaciones.Interfaces;
+using lib_dominio.Entidades;
+using lib_repositorios.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace lib_aplicaciones.Implementaciones
+{
+    public class EmpleadosAplicacion : IEmpleadosAplicacion
+    {
+        private IConexion? IConexion = null;
+
+        public EmpleadosAplicacion(IConexion iConexion)
+        {
+            this.IConexion = iConexion;
+        }
+
+        public void Configurar(string StringConexion)
+        {
+            this.IConexion!.StringConexion = StringConexion;
+        }
+
+        public Empleados? Borrar(Empleados? entidad)
+        {
+            if (entidad == null)
+                throw new Exception("lbFaltaInformacion");
+
+            if (entidad!.ID == 0)
+                throw new Exception("lbNoSeGuardo");
+
+            // Calculos
+
+            this.IConexion!.Empleados!.Remove(entidad);
+            this.IConexion.SaveChanges();
+            return entidad;
+        }
+
+        public Empleados? Guardar(Empleados? entidad)
+        {
+            if (entidad == null)
+                throw new Exception("lbFaltaInformacion");
+
+            if (entidad.ID != 0)
+                throw new Exception("lbYaSeGuardo");
+
+            // Calculos
+
+            this.IConexion!.Empleados!.Add(entidad);
+            this.IConexion.SaveChanges();
+            return entidad;
+        }
+
+        public List<Empleados> Listar()
+        {
+            return this.IConexion!.Empleados!.Take(20).ToList();
+        }
+
+        public List<Empleados> PorCodigo(Empleados? entidad)
+        {
+            return this.IConexion!.Empleados!
+                .Where(x => x.Nombre!.Contains(entidad!.Nombre!))
+                .ToList();
+        }
+
+        public Empleados? Modificar(Empleados? entidad)
+        {
+            if (entidad == null)
+                throw new Exception("lbFaltaInformacion");
+
+            if (entidad!.ID == 0)
+                throw new Exception("lbNoSeGuardo");
+
+            // Calculos
+
+            var entry = this.IConexion!.Entry<Empleados>(entidad);
+            entry.State = EntityState.Modified;
+            this.IConexion.SaveChanges();
+            return entidad;
+        }
+    }
+}
---
+++ lib_aplicaciones/Interfaces/IEmpleadosAplicaciones.cs
@@ -0,0 +1,14 @@
+﻿using lib_dominio.Entidades;
+
+namespace lib_aplicaciones.Interfaces
+{
+    public interface IEmpleadosAplicacion
+    {
+        void Configurar(string StringConexion);
+        List<Empleados> PorCodigo(Empleados? entidad);
+        List<Empleados> Listar();
+        Empleados? Guardar(Empleados? entidad);
+        Empleados? Modificar(Empleados? entidad);
+        Empleados? Borrar(Empleados? entidad);
+    }
+}

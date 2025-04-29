e97ff793f75db04c0d729190ad5dae0ca4ec06f2
mauricio <mauricioalejandrolarrea122@gmail.com>
2025-04-29 08:47:31 -0500

    Creacion Clases IEmpresas y Empresas en aplicaciones

---
+++ lib_aplicaciones/Interfaces/IEmpresasAplicaciones.cs
@@ -0,0 +1,14 @@
+﻿using lib_dominio.Entidades;
+
+namespace lib_aplicaciones.Interfaces
+{
+    public interface IEmpresasAplicacion
+    {
+        void Configurar(string StringConexion);
+        List<Empresas> PorCodigo(Empresas? entidad);
+        List<Empresas> Listar();
+        Empresas? Guardar(Empresas? entidad);
+        Empresas? Modificar(Empresas? entidad);
+        Empresas? Borrar(Empresas? entidad);
+    }
+}

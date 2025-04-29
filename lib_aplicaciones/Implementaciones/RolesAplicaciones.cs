cd2d4333a446a99247497c588d364c90bb3c3b47
mauricio <mauricioalejandrolarrea122@gmail.com>
2025-04-29 09:02:03 -0500

    Creacion RolesAplicaciones y IRoles

---
+++ lib_aplicaciones/Implementaciones/RolesAplicaciones.cs
@@ -0,0 +1,80 @@
+﻿using lib_aplicaciones.Interfaces;
+using lib_dominio.Entidades;
+using lib_repositorios.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace lib_aplicaciones.Implementaciones
+{
+    public class RolesAplicacion : IRolesAplicacion
+    {
+        private IConexion? IConexion = null;
+
+        public RolesAplicacion(IConexion iConexion)
+        {
+            this.IConexion = iConexion;
+        }
+
+        public void Configurar(string StringConexion)
+        {
+            this.IConexion!.StringConexion = StringConexion;
+        }
+
+        public Roles? Borrar(Roles? entidad)
+        {
+            if (entidad == null)
+                throw new Exception("lbFaltaInformacion");
+
+            if (entidad!.ID == 0)
+                throw new Exception("lbNoSeGuardo");
+
+            // Calculos
+
+            this.IConexion!.Roles!.Remove(entidad);
+            this.IConexion.SaveChanges();
+            return entidad;
+        }
+
+        public Roles? Guardar(Roles? entidad)
+        {
+            if (entidad == null)
+                throw new Exception("lbFaltaInformacion");
+
+            if (entidad.ID != 0)
+                throw new Exception("lbYaSeGuardo");
+
+            // Calculos
+
+            this.IConexion!.Roles!.Add(entidad);
+            this.IConexion.SaveChanges();
+            return entidad;
+        }
+
+        public List<Roles> Listar()
+        {
+            return this.IConexion!.Roles!.Take(20).ToList();
+        }
+
+        public List<Roles> PorCodigo(Roles? entidad)
+        {
+            return this.IConexion!.Roles!
+                .Where(x => x.Nombre!.Contains(entidad!.Nombre!))
+                .ToList();
+        }
+
+        public Roles? Modificar(Roles? entidad)
+        {
+            if (entidad == null)
+                throw new Exception("lbFaltaInformacion");
+
+            if (entidad!.ID == 0)
+                throw new Exception("lbNoSeGuardo");
+
+            // Calculos
+
+            var entry = this.IConexion!.Entry<Roles>(entidad);
+            entry.State = EntityState.Modified;
+            this.IConexion.SaveChanges();
+            return entidad;
+        }
+    }
+}

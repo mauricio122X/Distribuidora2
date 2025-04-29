a56a2b73c94ae0fe905785f52701415938acd89c
mauricio <mauricioalejandrolarrea122@gmail.com>
2025-04-29 08:58:31 -0500

    Creacion DocumentosAplicaciones y IDocumentos

---
+++ lib_aplicaciones/Implementaciones/DocumentosAplicaciones.cs
@@ -0,0 +1,80 @@
+﻿using lib_aplicaciones.Interfaces;
+using lib_dominio.Entidades;
+using lib_repositorios.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace lib_aplicaciones.Implementaciones
+{
+    public class DocumentosAplicacion : IDocumentosAplicacion
+    {
+        private IConexion? IConexion = null;
+
+        public DocumentosAplicacion(IConexion iConexion)
+        {
+            this.IConexion = iConexion;
+        }
+
+        public void Configurar(string StringConexion)
+        {
+            this.IConexion!.StringConexion = StringConexion;
+        }
+
+        public Documentos? Borrar(Documentos? entidad)
+        {
+            if (entidad == null)
+                throw new Exception("lbFaltaInformacion");
+
+            if (entidad!.ID == 0)
+                throw new Exception("lbNoSeGuardo");
+
+            // Calculos
+
+            this.IConexion!.Documentos!.Remove(entidad);
+            this.IConexion.SaveChanges();
+            return entidad;
+        }
+
+        public Documentos? Guardar(Documentos? entidad)
+        {
+            if (entidad == null)
+                throw new Exception("lbFaltaInformacion");
+
+            if (entidad.ID != 0)
+                throw new Exception("lbYaSeGuardo");
+
+            // Calculos
+
+            this.IConexion!.Documentos!.Add(entidad);
+            this.IConexion.SaveChanges();
+            return entidad;
+        }
+
+        public List<Documentos> Listar()
+        {
+            return this.IConexion!.Documentos!.Take(20).ToList();
+        }
+
+        public List<Documentos> PorCodigo(Documentos? entidad)
+        {
+            return this.IConexion!.Documentos!
+                .Where(x => x.Tipo_Movimiento!.Contains(entidad!.Tipo_Movimiento!))
+                .ToList();
+        }
+
+        public Documentos? Modificar(Documentos? entidad)
+        {
+            if (entidad == null)
+                throw new Exception("lbFaltaInformacion");
+
+            if (entidad!.ID == 0)
+                throw new Exception("lbNoSeGuardo");
+
+            // Calculos
+
+            var entry = this.IConexion!.Entry<Documentos>(entidad);
+            entry.State = EntityState.Modified;
+            this.IConexion.SaveChanges();
+            return entidad;
+        }
+    }
+}
---
+++ lib_aplicaciones/Interfaces/IDocumentosAplicaciones.cs
@@ -0,0 +1,14 @@
+﻿using lib_dominio.Entidades;
+
+namespace lib_aplicaciones.Interfaces
+{
+    public interface IDocumentosAplicacion
+    {
+        void Configurar(string StringConexion);
+        List<Documentos> PorCodigo(Documentos? entidad);
+        List<Documentos> Listar();
+        Documentos? Guardar(Documentos? entidad);
+        Documentos? Modificar(Documentos? entidad);
+        Documentos? Borrar(Documentos? entidad);
+    }
+}

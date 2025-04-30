9dbe943248971881aa55957f56de5cec747afd6e
mauricio <mauricioalejandrolarrea122@gmail.com>
2025-04-30 12:56:53 -0500

    Creacion Vehiculos_DocumentosAplicaciones y IVehiculos_Documentos

---
+++ lib_aplicaciones/Interfaces/IVehiculos_DocumentosAplicaciones.cs
@@ -0,0 +1,14 @@
+﻿using lib_dominio.Entidades;
+
+namespace lib_aplicaciones.Interfaces
+{
+    public interface IVehiculos_DocumentosAplicacion
+    {
+        void Configurar(string StringConexion);
+        List<Vehiculos_Documentos> PorCodigo(Vehiculos_Documentos? entidad);
+        List<Vehiculos_Documentos> Listar();
+        Vehiculos_Documentos? Guardar(Vehiculos_Documentos? entidad);
+        Vehiculos_Documentos? Modificar(Vehiculos_Documentos? entidad);
+        Vehiculos_Documentos? Borrar(Vehiculos_Documentos? entidad);
+    }
+}

a2d06e2e3c8e061436417e739b90fda048514592
mauricio <mauricioalejandrolarrea122@gmail.com>
2025-05-20 08:29:16 -0500

    Layout completo

---
+++ asp_presentaciones/Pages/Ventanas/Empresas.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace asp_presentaciones.Pages.Ventanas
+{
+    public class EmpresasModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
